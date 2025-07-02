52c7a9fb031f6fecc8e4a19b897fc07397a3abed
Helge Rausch <helge@rausch.io>
2025-07-02 09:06:46 +0200

    Set some more faces

--- alduin-theme.el
+++ alduin-theme.el
@@ -5,20 +5,32 @@
 
 (defvar alduin-default-colors
   '(("alduin-bg"             . "#121212")
-    ("alduin-fg"             . "#DFDFAF")
-    ("alduin-black"          . "#1C1C1C")
-    ("alduin-red"            . "#AF1600")
-    ("alduin-green"          . "#AF5F00")
+    ("alduin-fg"             . "#dfdfaf")
+    ("alduin-black"          . "#1c1c1c")
+    ("alduin-red"            . "#af1600")
+    ("alduin-green"          . "#af5f00")
     ("alduin-yellow"         . "#878787")
-    ("alduin-blue"           . "#AF875F")
-    ("alduin-magenta"        . "#875F5F")
-    ("alduin-cyan"           . "#87AFAF")
-    ("alduin-white"          . "#DFDFAF")
-    ("alduin-bold"           . "#FFFFFF")
-    ("alduin-white-bright"   . "#FFDF87")
-    ("alduin-magenta-bright" . "#AF8787")
+    ("alduin-blue"           . "#af875f")
+    ("alduin-magenta"        . "#875f5f")
+    ("alduin-cyan"           . "#87afaf")
+    ("alduin-white"          . "#dfdfaf")
+    ("alduin-bold"           . "#ffffff")
+    ("alduin-white-bright"   . "#ffdf87")
+    ("alduin-magenta-bright" . "#af8787")
     ("alduin-black-bright"   . "#87875f")
-    ("alduin-black-alt"      . "#262626")))
+    ("alduin-black-alt"      . "#262626")
+    ; original color names
+    ("orange"      . "#af875f")
+    ("soft-orange" . "#dfaf87")
+    ("dark-orange" . "#af5f00")
+    ("soft-red"    . "#af8787")
+    ("red"         . "#af5f5f")
+    ("dark-red"    . "#875f5f")
+    ("cyan"        . "#87afaf")
+    ("grey"        . "#878787")
+    ("green"       . "#87875f")
+    ("soft-yellow" . "#dfdfaf")
+    ("dark-grey"   . "#4e4e4e")))
 
 (defmacro alduin-apply-colors (&rest body)
   `(let ((foo "bar")
@@ -32,38 +44,45 @@
  (custom-theme-set-faces
   'alduin
   ;;;; default
-  `(default ((t (:foreground ,alduin-fg :background ,alduin-black))))
+  `(default ((t (:foreground ,soft-yellow :background ,alduin-black))))
   ;;;; strings
   `(font-lock-string-face ((t (:foreground ,alduin-white-bright
                                :background ,alduin-black-alt))))
   ;;;; functions
-  `(font-lock-function-name-face ((t (:foreground ,alduin-magenta))))
+  `(font-lock-function-name-face ((t (:foreground ,dark-red))))
   ;;;; constants
-  `(font-lock-type-face ((t (:foreground ,alduin-magenta-bright))))
+  `(font-lock-type-face ((t (:foreground ,soft-red))))
   ;;;; comments
-  `(font-lock-comment-face ((t (:foreground ,alduin-black-bright))))
-  `(font-lock-comment-delimiter-face ((t (:foreground ,alduin-black-bright))))
-  `(font-lock-doc-face ((t (:foreground ,alduin-black-bright))))
+  `(font-lock-comment-face ((t (:foreground ,green))))
+  `(font-lock-comment-delimiter-face ((t (:foreground ,green))))
+  `(font-lock-doc-face ((t (:foreground ,green))))
   ;;;; keywords (if etc)
-  `(font-lock-keyword-face ((t (:foreground ,alduin-yellow))))
+  `(font-lock-keyword-face ((t (:foreground ,grey))))
   ;;;; variables
-  `(font-lock-variable-name-face ((t (:foreground ,alduin-cyan))))
+  `(font-lock-variable-name-face ((t (:foreground ,cyan))))
   ;;;; mode line
-  `(mode-line ((t (:foreground ,alduin-bg :background ,alduin-black-bright :box t :overline nil))))
-  `(mode-line-inactive ((t (:foreground ,alduin-black-bright :background ,alduin-bg :box t :overline nil))))
+  `(mode-line ((t (:foreground ,alduin-bg :background ,green :box t :overline nil))))
+  `(mode-line-inactive ((t (:foreground ,green :background ,alduin-bg :box t :overline nil))))
   ;;;; fringe
   `(fringe ((t (:background ,alduin-black))))
   ;;;; hl line
   `(hl-line ((t (:background ,alduin-black-alt))))
   ;;;; dired
-  `(dired-directory ((t (:foreground ,alduin-magenta))))
-  `(dired-symlink ((t (:foreground ,alduin-yellow))))
+  `(dired-directory ((t (:foreground ,dark-red))))
+  `(dired-symlink ((t (:foreground ,grey))))
   ;;;; magit
   `(magit-section-highlight ((t (:background ,alduin-black-alt))))
   `(magit-diff-file-heading-highlight ((t (:background ,alduin-black-alt))))
   `(magit-diff-context-highlight ((t (:background ,alduin-black-alt))))
   ;;;; links
-  `(link ((t (:foreground ,alduin-magenta))))))
+  `(link ((t (:foreground ,red))))
+  `(markdown-inline-code-face ((t (:foreground ,soft-orange))))
+  `(markdown-header-face ((t (:foreground ,soft-orange))))
+  `(markdown-header-delimiter-face ((t (:foreground ,soft-yellow))))
+  `(line-number ((t (:foreground ,dark-grey
+                     :background ,alduin-bg))))
+  `(isearch ((t (:foreground ,soft-yellow
+                 :background ,soft-red))))))
 
 ;;;###autoload
 (and load-file-name
